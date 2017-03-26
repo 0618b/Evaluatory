dc9eccc7f4a6122005c7e57fd7e6c2578fd50934
gameweng <jchanchai01@gmail.com>
2017-03-26 15:42:34 +0700

    issues when binding and angular with html pages

--- public/app/controllers/evalformsControllers.js
+++ public/app/controllers/evalformsControllers.js
@@ -1,5 +1,36 @@
 angular.module('evalformsControllers', ['evalformsServices'])
-    .controller('evalformsControllers', function() {
-        console.log('Testing');
-    });
+    .controller('evalformsControllers', function(EvalForm, $scope) {
+            var app = this;
+
+            app.editAndDeleteAccess = false;
+            app.errorMsg = false;
+            app.showMoreError = false;
+
+            function getEvalForms() {
+                EvalForm.getEvalForms().then(function(data) {
+                    console.log('GETTER');
+                    if(data.data.success) {
+                        if(data.data.permission === 'admin') {
+                            app.evalforms = data.data.evalforms;
+                            app.editAndDeleteAccess = true;
+                        } else {
+                            app.editAndDeleteAccess = false;
+                            app.errorMsg = data.data.message;
+                        }
+                    }
+                });
+            }
+
+            getEvalForms();
+
+            app.deleteEvalForms() = function(id) {
+                EvalForm.deleteEvalForms(id).then(function(data) {
+                    if (data.data.success) {
+                        getEvalForms();
+                    } else {
+                        app.showMoreError = data.data.message;
+                    }
+                });
+            };  
+        });
 
