5b8521d88ed499128ed6615266f3be7862bcc4f0
gameweng <jchanchai01@gmail.com>
2017-04-20 00:24:11 +0700

    fixed maximum call stack

--- public/app/controllers/selftempsControllers.js
+++ public/app/controllers/selftempsControllers.js
@@ -12,13 +12,10 @@ angular.module('selftempsControllers', ['selftempsServices'])
         this.clone = function() {
             SelfTemplate.cloneSelfTemplate($scope.template); // Clone a template to create a new object
             var cloneObj = $scope.templateData.slice(-1)[0]; // Select the last element of database to use it as a cloned object
-            refresh(); 
+            refresh();
                 $location.url('/selftemps/' + cloneObj._id);
-                    SelfTemplate.getSelfTemplateById($routeParams.id).then(function(data) {
+                    SelfTemplate.getSelfTemplateById({_id: $routeParams.id}, function(selftemp){
                         console.log($routeParams.id);
                     })
                 }
-        
-
-
     })
--- public/app/routes.js
+++ public/app/routes.js
@@ -4,27 +4,24 @@ angular.module('mainRoutes', ['ngRoute'])
         //AngularJS route handler
         $routeProvider
 
-        .when('/', {
-            templateUrl: 'app/views/pages/homepage/home.html'
-        })
-        .when('/evalforms', {
-            templateUrl: 'app/views/pages/evalforms/evalforms.html'
+        .when('/home', {
+            templateUrl: '/app/views/pages/homepage/home.html'
         })
         .when('/selftemps', {
-            templateUrl: 'app/views/pages/evalforms/selftemp.html',
+            templateUrl: '/app/views/pages/evalforms/selftemp.html',
             controller: 'selftempsCtrl',
             controllerAs: 'stCtrl'
         })
         .when('/selftemps/:id', {
-            templateUrl: 'app/views/pages/evalforms/st-eval.html',
+            templateUrl: '/app/views/pages/evalforms/st-eval.html',
             controller: 'selftempsCtrl',
             controllerAs: 'stCtrl'
         })
         .when('/othertemps', {
-            templateUrl: 'app/views/pages/evalforms/othertemp.html'
+            templateUrl: '/app/views/pages/evalforms/othertemp.html'
         })
         .otherwise({
-            redirectTo: '/'
+            redirectTo: '/home'
         });
 
         $locationProvider.html5Mode({
