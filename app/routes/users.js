a3eb0105ba44916b80a9ff9cc3c328c49412d119
gameweng <jchanchai01@gmail.com>
2017-04-29 11:14:39 +0700

    user api

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,30 +2,6 @@ var User = require('../models/users');
 
 module.exports = function(router) {
 
-    // get all data
-    router.get('/users', function(req, res) {
-        User.find({}, function(err, data) {
-            if (err) {
-                res.send('Error');
-            } else {
-                res.json(data);
-            }
-        });
-    });
-
-    // get user profile only one
-    router.get('/users/:id', function(req, res) {
-        var id = req.params.id;
-        User.find({ _id: id }, function(err, data) {
-            if (err) {
-                res.send('Error');
-            } else {
-                res.json(data[0]);
-            }
-        });
-    });
-
-    // create new user
     router.post('/users', function(req, res) {
         var u = new User();
         u.username = req.body.username;
@@ -35,11 +11,14 @@ module.exports = function(router) {
         u.position = req.body.position;
         u.positionLevel = req.body.positionLevel;
         u.positionCategory = req.body.positionCategory;
-        u.positionNumber = req.body.positionNumber;
         u.belongTo = req.body.belongTo;
         u.subjectGroup = req.body.subjectGroup;
         u.workGroup = req.body.workGroup;
         u.classGroup = req.body.classGroup;
+        u.permission = req.body.permission;
+        u.subjectGroupRole = req.body.subjectGroupRole;
+        u.workGroupRole = req.body.workGroupRole;
+        u.classGroupRole = req.body.classGroupRole;
 
         if (req.body.username === null || req.body.username === '' || req.body.password === null || req.body.password === '' ||
             req.body.firstName === null || req.body.firstName === '' || req.body.lastName === null || req.body.lastName === '' ||
@@ -47,67 +26,67 @@ module.exports = function(router) {
             req.body.positionCategory === null || req.body.positionCategory === '' || req.body.positionNumber === null || req.body.positionNumber === '' ||
             req.body.belongTo === null || req.body.belongTo === '' || req.body.subjectGroup === null || req.body.subjectGroup === '' ||
             req.body.workGroup === null || req.body.workGroup === '' || req.body.classGroup === null || req.body.classGroup === '') {
-            res.send('กรุณากรอกข้อมูลให้ครบทุกช่อง');
+            res.json({
+                success: false,
+                msg: 'กรุณากรอกข้อมูลให้ครบทุกช่อง'
+            });
         } else {
             u.save(function(err) {
                 if (err) {
-                    res.send('Error');
+                    res.json({
+                        success: false,
+                        msg: 'มีบางอย่างผิดพลาด'
+                    });
                 } else {
-                    res.send('Created!');
+                    res.json({
+                        success: true,
+                        msg: 'สำเร็จ'
+                    });
                 }
             });
         }
     });
 
-    // delete user
-    router.delete('/users/:id', function(req, res) {
-        var id = req.params.id;
-        User.findByIdAndRemove(id, function(err) {
+    router.get('/users', function(req, res, next) {
+        User.find({}, function(err, users) {
             if (err) {
-                res.send('Error');
+                return next(err);
             } else {
-                res.send('Deleted!');
+                res.json(users);
             }
         });
     });
 
-    // update user
-    router.put('/users/:id', function(req, res) {
-        var id = req.params.id;
-        var obj = req.body;
-        User.findByIdAndUpdate(id, {
-                username: obj.username,
-                password: obj.password,
-                firstName: obj.firstName,
-                lastName: obj.lastName,
-                position: obj.position,
-                positionLevel: obj.positionLevel,
-                positionCategory: obj.positionCategory,
-                positionNumber: obj.positionNumber,
-                belongTo: obj.belongTo,
-                subjectGroup: obj.subjectGroup,
-                workGroup: obj.workGroup,
-                classGroup: obj.classGroup,
-                permission: obj.permission,
-                subjectGroupRole: obj.subjectGroupRole,
-                workGroupRole: obj.workGroupRole,
-                classGroupRole: obj.classGroupRole
-            },
-            function(err) {
-                if (err) {
-                    res.send('Error');
-                } else {
-                    res.send('Updated!');
-                }
-            });
+    router.get('/users/:id', function(req, res, next) {
+        User.findOne({ _id: req.params.id }, function(err, user) {
+            if (err) {
+                return next(err);
+            } else {
+                res.json(user);
+            }
+        });
     });
 
-    router.use(function(req, res, next) {
-        var err = new Error('Not Found');
-        err.status = 404;
-        next(err);
+    router.delete('/users/:id', function(req, res, next) {
+        User.findOneAndRemove({ _id: req.params.id }, function(err) {
+            if (err) {
+                return next(err);
+            } else {
+                res.json('Deleted!');
+            }
+        });
+    });
+
+    router.put('/users/:id', function(req, res, next) {
+        User.findOneAndUpdate({ _id: req.params.id }, req.body, function(err, user) {
+            if (err) {
+                return next(err);
+            } else {
+                res.json(user);
+            }
+        });
     });
 
     return router;
 
-}
+};
