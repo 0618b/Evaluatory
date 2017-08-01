acb6af6fa6b35c78d613602261c60ce5a7bc399e
gameweng <jchanchai01@gmail.com>
2017-08-02 00:38:55 +0700

    add more fields

--- app/models/othertemplates.js
+++ app/models/othertemplates.js
@@ -5,8 +5,11 @@ var otherTemplateSchema = new Schema({
     header: {},
     other_template: {},
     totalScore: { type: String, default: 0 },
+    created: { type: Date, default: Date.now },
+    updated: { type: Date },
     isCloned: { type: Boolean, default: false },
-    isSubmitted: { type: Boolean, default: false }
+    isSubmitted: { type: Boolean, default: false },
+    evaluatedBy: { type: String, ref: 'User' }
 });
 
 module.exports = mongoose.model('OtherTemplate', otherTemplateSchema);
--- app/models/selftemplates.js
+++ app/models/selftemplates.js
@@ -5,6 +5,8 @@ var selfTemplateSchema = new Schema({
     header: {},
     self_template: {},
     totalScore: { type: String, default: 0 },
+    created: { type: Date, default: Date.now },
+    updated: { type: Date },
     isCloned: { type: Boolean, default: false },
     isSubmitted: { type: Boolean, default: false },
     evaluatedBy: { type: String, ref: 'User' }
