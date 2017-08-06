483856e8ab1f367362b3107c3f158f7bde062fbf
gameweng <jchanchai01@gmail.com>
2017-08-06 23:31:31 +0700

    we can bring data to frontend & change a little bit of model

--- app/models/selftemplates.js
+++ app/models/selftemplates.js
@@ -1,12 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var today = new Date();
+var date = today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear();
+
 
 var selfTemplateSchema = new Schema({
     header: {},
     self_template: {},
     totalScore: { type: String, default: 0 },
-    created: { type: Date, default: Date.now },
-    updated: { type: Date },
+    created: { type: String, default: date },
+    updated: { type: String, default: date },
     isCloned: { type: Boolean, default: false },
     isSubmitted: { type: Boolean, default: false },
     evaluatedBy: { type: String, ref: 'User' }
--- public/app/controllers/selftempsControllers.js
+++ public/app/controllers/selftempsControllers.js
@@ -1,49 +1,6 @@
 angular.module('selftempsControllers', ['selftempsServices'])
     .controller('selftempsCtrl', function(selfTemplateService, $scope, $location, $routeParams, $rootScope, $timeout) {
 
-        /*function getAllSelfTemplates() {
-            selfTemplateService.getAllSelfTemplates().then(function(data) {
-                $scope.template = data.data[0];
-                $scope.templateData = data.data;
-            });
-        };
-
-        getAllSelfTemplates(); //invoke function to get data from databases
-
-        $scope.clone = function() {
-            selfTemplateService.cloneSelfTemplate($scope.template); // Clone a template to create a new object
-            var cloneObj = $scope.templateData.slice(-1)[0]; // Select the last element of database to use it as a cloned object
-            $location.url('/selftemps/' + cloneObj._id); // redirect to the evaluation page with an id
-            cloneObj.isCloned = true;
-            $routeParams.id = cloneObj._id;
-        };*/
-
-        function getEachSelfTemplates() {
-            selfTemplateService.getEachSelfTemplates().then(function(data) {
-                console.log(data);
-                if (!data) {
-                    swal({
-                        title: 'ยังไม่มีแบบประเมินเลย',
-                        text: 'ต้องการเริ่มสร้างแบบประเมินหรือไม่ ?',
-                        type: 'question',
-                        showCancelButton: true,
-                        confirmButtonColor: '#428bca',
-                        cancelButtonColor: '#d9534f',
-                        confirmButtonText: 'ใช่แล้ว',
-                        cancelButtonText: 'ยังก่อน'
-                    }).then(function() {
-                        $scope.clone();
-                        swal(
-                            'สร้างแบบประเมินเรียบร้อยแล้ว',
-                            'success'
-                        )
-                    })
-                }
-            });
-        };
-
-        getEachSelfTemplates();
-
         $scope.clone = function() {
             selfTemplateService.getAllSelfTemplates().then(function(data) {
                 var selftemplate = data.data[0];
@@ -68,8 +25,20 @@ angular.module('selftempsControllers', ['selftempsServices'])
                     cloneObj.isCloned = true;
                     $routeParams.id = cloneObj._id;*/
             })
-
         }
+
+        var openCreateSelfTempModal = function() {
+            $("#createStModal").modal('show')
+        }
+
+        function getEachSelfTemplates() {
+            selfTemplateService.getEachSelfTemplates().then(function(data) {
+                $scope.selftemplateData = data.data;
+                if (data.data.length < 1) openCreateSelfTempModal();
+            });
+        };
+
+        getEachSelfTemplates();
 
     })
 
