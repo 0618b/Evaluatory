b8f74b9362809cbfd84f0d9aba1428743e7a651a
gameweng <jchanchai01@gmail.com>
2017-10-28 15:48:59 +0700

    กำลังหาวิธีแบ่งรอบการประเมินด้วย mongoose

--- app/models/selftemplates.js
+++ app/models/selftemplates.js
@@ -1,13 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var present = new Date();
+var month = present.getMonth() + 1;
+var year = present.getFullYear() + 543;
 
 var selfTemplateSchema = new Schema({
-    header: {},
     self_template: {},
     totalScore: { type: Number, default: 0 },
     isCloned: { type: Boolean, default: false },
     isSubmitted: { type: Boolean, default: false },
-    evaluatedBy: { type: String, ref: 'User' }
+    evaluatedBy: { type: String, ref: 'User' },
+    timestamp: {
+        month: { type: String, default: month },
+        year: { type: String, default: year }
+    }
 });
 
 module.exports = mongoose.model('SelfTemplate', selfTemplateSchema);
