4a8a78dbbf980eafbe44e6ab5fe7f57df16065e4
gameweng <jchanchai01@gmail.com>
2017-11-11 15:43:36 +0700

    มันแปลกๆ filter ได้แค่บางส่วน แถมตัว frontend ยังเออเร่อ

--- public/app/controllers/selftempsControllers.js
+++ public/app/controllers/selftempsControllers.js
@@ -40,76 +40,88 @@ angular.module('selftempsControllers', ['selftempsServices', 'angular.filter'])
 
 .controller('selfevalCtrl', function(selfTemplateService, $scope, $location, $routeParams, $rootScope, $timeout) {
 
-        function getSelfTemplateById(id) {
-            selfTemplateService.getSelfTemplateById($routeParams.id).then(function(data) {
-                if (data.status === 200) { // check that data is OK
-                    $scope.data = JSON.parse(JSON.stringify(data)); //parse data into json strings to show in the system
-                    $scope.self_template = data.data.self_template;
-                } else {
-                    swal({
-                        title: 'มีบางอย่างผิดพลาด',
-                        type: 'warning',
-                        timer: 2000
-                    })
-                }
-            });
-        }
-
-        getSelfTemplateById();
-
-        $scope.evalSelfTemp = function() {
-            var evalData = {
-                    "self_template": $scope.self_template,
-                    "header": $scope.header
-                } // saving the eval data then parse as an object
-            selfTemplateService.evalSelfTemplate($routeParams.id, evalData).then(function(data) {
+    function getSelfTemplateById(id) {
+        selfTemplateService.getSelfTemplateById($routeParams.id).then(function(data) {
+            if (data.status === 200) { // check that data is OK
+                $scope.data = JSON.parse(JSON.stringify(data)); //parse data into json strings to show in the system
+                $scope.self_template = data.data.self_template;
+            } else {
                 swal({
-                    title: 'บันทึกผลการประเมินเรียบร้อยแล้ว',
-                    type: 'success',
+                    title: 'มีบางอย่างผิดพลาด',
+                    type: 'warning',
                     timer: 2000
                 })
-                $timeout(function() {
-                    $location.url('/selftemps')
-                }, 500);
-            });
-        };
-
-        $scope.viewScore = function() {
-            $location.url('/selfscores/' + $routeParams.id);
-        }
-
-    }).directive('convertToNumber', function() {
-        return {
-            require: 'ngModel',
-            link: function(scope, element, attrs, ngModel) {
-                ngModel.$parsers.push(function(val) {
-                    return parseInt(val, 10);
-                });
-                ngModel.$formatters.push(function(val) {
-                    return '' + val;
-                });
             }
-        };
-    })
-    /*.filter('unique', function() {
-        return function(collection, primaryKey) { //no need for secondary key
-            var output = [],
-                keys = [];
-            var splitKeys = primaryKey.split('.'); //split by period
+        });
+    }
 
-            angular.forEach(collection, function(item) {
-                var key = {};
-                angular.copy(item, key);
-                for (var i = 0; i < splitKeys.length; i++) {
-                    key = key[splitKeys[i]]; //the beauty of loosely typed js :)
-                }
+    getSelfTemplateById();
 
-                if (keys.indexOf(key) === -1) {
-                    keys.push(key);
-                    output.push(item);
-                }
+    $scope.evalSelfTemp = function() {
+        var evalData = {
+                "self_template": $scope.self_template,
+                "header": $scope.header
+            } // saving the eval data then parse as an object
+        selfTemplateService.evalSelfTemplate($routeParams.id, evalData).then(function(data) {
+            swal({
+                title: 'บันทึกผลการประเมินเรียบร้อยแล้ว',
+                type: 'success',
+                timer: 2000
+            })
+            $timeout(function() {
+                $location.url('/selftemps')
+            }, 500);
+        });
+    };
+
+    $scope.viewScore = function() {
+        $location.url('/selfscores/' + $routeParams.id);
+    }
+
+}).directive('convertToNumber', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(val) {
+                return parseInt(val, 10);
             });
+            ngModel.$formatters.push(function(val) {
+                return '' + val;
+            });
+        }
+    };
+}).filter('unique', function() {
+    return function(input, key) {
+        var unique = {};
+        var uniqueList = [];
+        for (var i = 0; i < input.length; i++) {
+            if (typeof unique[input[i][key]] == "undefined") {
+                unique[input[i][key]] = "";
+                uniqueList.push(input[i]);
+            }
+        }
+        return uniqueList;
+    };
+});
+/*.filter('unique', function() {
+    return function(collection, primaryKey) { //no need for secondary key
+        var output = [],
+            keys = [];
+        var splitKeys = primaryKey.split('.'); //split by period
 
-            return output;
-        };
-    });*/
+        angular.forEach(collection, function(item) {
+            var key = {};
+            angular.copy(item, key);
+            for (var i = 0; i < splitKeys.length; i++) {
+                key = key[splitKeys[i]]; //the beauty of loosely typed js :)
+            }
+
+            if (keys.indexOf(key) === -1) {
+                keys.push(key);
+                output.push(item);
+            }
+        });
+
+        return output;
+    };
+});*/
