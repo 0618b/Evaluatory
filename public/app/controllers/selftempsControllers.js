60e3560fcf00700c9f072df08a0a8622af243f3f
gameweng <jchanchai01@gmail.com>
2017-11-22 11:50:26 +0700

    ทำตัวคำนวณผลคะแนนได้แล้ว

--- public/app/controllers/selftempsControllers.js
+++ public/app/controllers/selftempsControllers.js
@@ -46,8 +46,42 @@ angular.module('selftempsControllers', ['selftempsServices', 'angular.filter'])
                 $scope.data = JSON.parse(JSON.stringify(data)); //parse data into json strings to show in the system
                 $scope.self_template = data.data.self_template;
 
-                console.log($scope.sumScore);
-                console.log($scope.sumWeight);
+                s0 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[0].score;
+                s1 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[1].score;
+                s2 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[2].score;
+                s3 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[3].score;
+                s4 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[4].score;
+                s5 = $scope.self_template.sectionGroup[0].choiceGroupList[1].choiceList[0].score;
+                s6 = $scope.self_template.sectionGroup[0].choiceGroupList[1].choiceList[1].score;
+                s7 = $scope.self_template.sectionGroup[0].choiceGroupList[1].choiceList[2].score;
+                s8 = $scope.self_template.sectionGroup[0].choiceGroupList[2].choiceList[0].score;
+                s9 = $scope.self_template.sectionGroup[0].choiceGroupList[2].choiceList[1].score;
+
+                w0 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[0].evalWeight;
+                w1 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[1].evalWeight;
+                w2 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[2].evalWeight;
+                w3 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[3].evalWeight;
+                w4 = $scope.self_template.sectionGroup[0].choiceGroupList[0].choiceList[4].evalWeight;
+                w5 = $scope.self_template.sectionGroup[0].choiceGroupList[1].choiceList[0].evalWeight;
+                w6 = $scope.self_template.sectionGroup[0].choiceGroupList[1].choiceList[1].evalWeight;
+                w7 = $scope.self_template.sectionGroup[0].choiceGroupList[1].choiceList[2].evalWeight;
+                w8 = $scope.self_template.sectionGroup[0].choiceGroupList[2].choiceList[0].evalWeight;
+                w9 = $scope.self_template.sectionGroup[0].choiceGroupList[2].choiceList[1].evalWeight;
+
+                var score_arr = [s0, s1, s2, s2, s4, s5, s6, s7, s8, s9];
+                var evalWeight_arr = [w0, w1, w2, w3, w4, w5, w6, w7, w8, w9];
+
+                $scope.totalScore = 0;
+                $scope.totalWeight = 0
+
+                for (var i = 0; i < score_arr.length; i++) {
+                    $scope.totalScore += score_arr[i];
+                }
+
+                for (var i = 0; i < evalWeight_arr.length; i++) {
+                    $scope.totalWeight += evalWeight_arr[i];
+                }
+
             } else {
                 swal({
                     title: 'มีบางอย่างผิดพลาด',
